febe99736b9e1d231fc6c0fb5e0c33805a22528e
CHZEE93 <zndn887@gmail.com>
2025-03-03 22:01:00 +0900

    SETTING

---
+++ projectj_back/src/main/java/com/projectj/projectj_back/projectj_back/ProjectjBackApplication.java
@@ -0,0 +1,13 @@
+package com.projectj.projectj_back.projectj_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectjBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectjBackApplication.class, args);
+	}
+
+}
---
+++ projectj_back/src/test/java/com/projectj/projectj_back/projectj_back/ProjectjBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectj.projectj_back.projectj_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
